cd12ccac70ed7bdcb52aa45dca1c7911ce20f67e
DarianMD <darianmanasturean@gmail.com>
2021-01-18 15:51:49 +0100

    Hub

---
+++ animacions.java
@@ -0,0 +1,53 @@
+
+public class animacions {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+	
+		
+		
+		System.out.println("d8888b. d88888b d888888b d8888b.  .d88b.  d8888b. db    db db    ");
+		Thread.sleep(200);
+		System.out.println("88  `8D 88'     `~~88~~' 88  `8D .8P  Y8. 88  `8D `8b  d8' 88     ");
+		Thread.sleep(200);
+		System.out.println("88oobY' 88ooooo    88    88oobY' 88    88 88oooY'  `8bd8'  88     ");
+		Thread.sleep(200);
+		System.out.println("88`8b   88~~~~~    88    88`8b   88    88 88~~~b.    88    88     ");
+		System.out.println("88 `88. 88.        88    88 `88. `8b  d8' 88   8D    88    88booo.");
+		Thread.sleep(500);
+		System.out.println("88   YD Y88888P    YP    88   YD  `Y88P'  Y8888P'    YP    Y88888P");
+		Thread.sleep(500);
+		System.out.println("\nCreated By :");
+		Thread.sleep(200);
+		System.out.println("Darian Manasturean");
+		Thread.sleep(200);
+		System.out.println("Gabriel Voica");
+		Thread.sleep(200);
+		System.out.println("Sergio Martín");
+		System.out.println("LOADING HUB...\n");
+		Thread.sleep(1000);
+		System.out.println("LOADING RESOURCES...0%\n");
+		Thread.sleep(300);
+		System.out.println("LOADING RESOURCES...10%\n");
+		Thread.sleep(1300);
+		System.out.println("LOADING RESOURCES...19%\n");
+		Thread.sleep(1000);
+		System.out.println("LOADING RESOURCES...43%\n");
+		Thread.sleep(1200);
+		System.out.println("LOADING RESOURCES...67%\n");
+		Thread.sleep(1500);
+		System.out.println("LOADING RESOURCES...97%\n");
+		Thread.sleep(900);
+		System.out.println("DONE!...100%\n");
+		Thread.sleep(500);
+		System.out.println("\n\n\n\n\n");
+		
+
+		
+
+		
+	
+		
+		
+}}
