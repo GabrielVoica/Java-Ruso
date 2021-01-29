ceac65d68e3566df26a549d915ace5f94fbb3bd6
Gabri <gabi_voica@hotmail.com>
2021-01-29 16:38:15 +0100

    done

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Retrobyl_Package"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"}]
--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Retrobyl_Package","l":"Retrobyl"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"HiperShoot"}]
