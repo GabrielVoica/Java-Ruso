60f8e84f91ee5574faacb78b558805418487abd6
Gabri <gabi_voica@hotmail.com>
2021-01-29 16:41:00 +0100

    donee

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"HiperShoot"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Chernoville"},{"p":"<Unnamed>","l":"HiperShoot"},{"p":"<Unnamed>","l":"JuegoNuclearCare"},{"p":"<Unnamed>","l":"RussianRoulette"},{"p":"<Unnamed>","l":"Terminal"}]
